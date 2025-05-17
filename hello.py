8667df0f9ae5fb8165eed1138e652bc9a05b33e8
dimitarbayraktarov <dimitarbayraktarov@hotmail.com>
2025-05-17 18:19:25 +0100

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,7 @@
+# Ask user for their name
+name = input("What's your name? ").strip().title()
+# Split user's name into first and last name
+first, last = name.split(" ")
+# Say hello to user
+print(f"Hello, {first}")
+
